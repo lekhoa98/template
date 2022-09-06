86d889315841bc446440b6ffbe4071e84e48d940
lekhoa98kl <khoalda@a-livesystems.com>
2022-09-06 17:24:54 +0700

    update

---
+++ dist/life-style/index.php
@@ -0,0 +1,68 @@
+<?php
+include_once(dirname(__DIR__) . '/app_config.php');
+include(APP_PATH . 'libs/head.php');
+?>
+<meta http-equiv="expires" content="86400">
+<link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/page/lifestyle.min.css">
+</head>
+
+<body>
+    <?php include(APP_PATH . 'libs/header.php'); ?>
+    <main class="container">
+        <div class="bg">
+            <h1 class="title"><span>GL BASEがある暮らし</span></h1>
+        </div>
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="https://www.google.com.vn">TOP</a></li>
+                <li>GL BASEがある暮らし</li>
+            </ul>
+        </div>
+        <div class="section">
+            <div class="firstBlock">
+                <div class="txt">
+                    ダミーキャンピングカーの本場ヨーロッパのデザイン感覚を基準にして、 <br>
+                    デザインと機能をさらに高い次元で融合させながら<br>
+                    日本の風土に合ったキャンピングカーを業界に先駆けて開発することを常に心がけてきました。
+                </div>
+                <div class="firstBlock__inside">
+                    <div class="bg-btn">
+                        <div class="bg-btn__ttl">
+                            <span>趣味を思いっきり楽しめる！</span><br>
+                            ロマンのある暮らし
+                        </div>
+                        <div class="grBtn">
+                            <a href="">キャンプ</a>
+                            <a href="">フィッシング</a>
+                        </div>
+                    </div>
+                    <div class="bg-btn">
+                        <div class="bg-btn__ttl">
+                            <span>一人で落ち着く時間も大切にしたい</span><br>
+                            自分の空間がある暮らし
+                        </div>
+                        <div class="grBtn">
+                            <a href="">自分だけの書斎</a>
+                            <a href="">避難生活</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="bgContent">
+                    <div class="bgContent__ttl">
+                        <span>趣味を思いっきり楽しめる！</span><br>
+                        <h2>ロマンのある暮らし</h2>
+                    </div>
+                    <div class="bgContent__txt">
+                    キャンプ・釣り・サイクリング・ハイキング・サーフィン・ラフティングなど、<br>
+                    GL BASEとハイエースはアウトドアに持ってこいの組み合わせ。<br>
+                    耐久性も申し分なく、どこへ遊びにいくにも役立つ“秘密基地”になってくれるでしょう。
+                    </div>
+                </div>
+                <div class="content content--01">
+                
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php include(APP_PATH . 'libs/footer.php'); ?>
+</body>
