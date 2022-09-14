40107c1cef26f6f00d7c1f29b45ca5a33ae3964e
lekhoa98kl <khoalda@a-livesystems.com>
2022-09-14 16:39:30 +0700

    update

--- dist/life-style/index.php
+++ dist/life-style/index.php
@@ -101,23 +101,24 @@ include(APP_PATH . 'libs/head.php');
                     <div class="wcm-content">
                         <div class="wcm-content__inside">
                             <div class="wcm-content__inside--ttl">
-                                キャンプライフ<br>
-                                自由で快適な
+                                どこまでも遠くへ<br>
+                                釣り場を目指して
                             </div>
                             <div class="wcm-content__inside--txt">
-                                カンカンカン…<br>
-                                ペグを打ち付ける音が心地よく鳴り響く。<br><br>
-                                腰を上げてふと目線を上にやると<br>
-                                青々とした樹々が僕たちをのぞき込んでいる。<br><br>
-                                パシャッ！<br>
-                                何だ？<br>
-                                水鉄砲を持った子どもたちがこちらを見て笑っている。<br><br>
-                                「さあ、そろそろ火を起こすから見にくるか？」<br><br>
-                                車には、道の駅で買い出しした野菜たちを手に取り<br>
-                                ニンマリとする妻の姿。<br><br><br>
-                                キャンプはいつだって楽しい。<br><br>
-                                でもGL BASEがあれば<br>
-                                もっと楽しく、もっと快適になる。
+                                窓の外には朝陽が差し込みキラキラと揺れる水面。<br>
+                                今日の釣り場は自分一人のようだ。<br><br>
+                                いつものプレイリストを聴きながら<br>
+                                ルアーを選ぶこの時間が心地よい。<br><br>
+                                スタートから３時間。<br><br>
+                                バケツには可愛らしいサイズのが２匹いるが<br>
+                                まだ本命に出会えていない。<br><br>
+                                さあ、日差しも強くなってきた。<br><br>
+                                車でちょっと腹ごしらえしたら<br>
+                                もう一本の竿で後半戦スタートだ。<br><br>
+                                魚と向き合う。<br>
+                                自分と向き合う。<br><br>
+                                GL BASEはいつも<br>
+                                この穏やかな世界に連れていってくれる。
                             </div>
                         </div>
                     </div>
@@ -125,19 +126,139 @@ include(APP_PATH . 'libs/head.php');
                 <div class="wcm">
                     <div class="content-Org">
                         <h4 class="content-Org__heading">
-                            GL BASEでキャンプがこんなに快適に
+                            GL BASEで釣りがもっとおもしろく
                         </h4>
                         <ul class="content-Org__txt">
-                            <li>かさばる道具の数々も、ハイエースなら荷室に積み込みラクラク。</li>
-                            <li>テントを張りにくい現場でも拠点をつくれる。</li>
-                            <li>車内に簡易トイレを設置できるから、家族連れでも安心。</li>
-                            <li>シートを倒せばテーブルが出現し、食卓にもなる。</li>
-                            <li>フルフラットモードにすれば車内で宿泊可能。</li>
+                            <li>竿の積み込みが楽</li>
+                            <li>細々とした道具も、キャビネットにうまく収納できて機能性抜群</li>
+                            <li>日陰を確保しにくい場所でも、拠点があると休憩時に安心。</li>
+                            <li>海岸沿いに車を乗りつけ、荷室に座りながら引きを待つのもロマン</li>
+                            <li>シート背面テーブルを作業台にして、釣った魚をその場で捌ける</li>
+                            <li>防水加工、汚れても拭き取りやすい材質だからお手入れ簡単</li>
                         </ul>
                     </div>
                 </div>
+                <div class="bgContent">
+                    <div class="bgContent__ttl">
+                        <span>じっくり落ち着く時間も大切にしたい</span><br>
+                        <h2>自分の空間がある暮らし</h2>
+                    </div>
+                    <div class="bgContent__txt">
+                        デスクワークにも読書にも、気分を変えて集中したいときの書斎として。<br>
+                        災害など有事の際には、家族を助けてくれるプライベート空間として<br>
+                        「個」を大切にしたいあなたのための、新しいかたちの“離れ”がある安心感。
+                    </div>
+                </div>
+                <div class="content content--03">
+                    <div class="wcm-content">
+                        <div class="wcm-content__inside">
+                            <div class="wcm-content__inside--ttl">
+                                書斎として<br>
+                                集中したいときの
+                            </div>
+                            <div class="wcm-content__inside--txt">
+                                「いってきまーす！」とドアを閉め<br>
+                                通勤時間５秒の自宅オフィスへ出勤完了。<br><br>
+                                タンブラーに入れたコーヒーを一口飲み、<br>
+                                ノートPCを開き、メールチェックから仕事が始まる。<br><br>
+                                そう、私のオフィスは<br>
+                                駐車場に停めてある我が家のハイエースだ。<br><br>
+                                家を建てる時は<br>
+                                自分の書斎をつくるほどでもないと思っていた。<br>
+                                しかしリモートワークが定着したこのご時世となっては<br>
+                                自宅のワークスペースの重要性は大きい。<br><br>
+                                集中できる空間をつくるために<br>
+                                家をリフォームするのは難しかったが、<br>
+                                ハイエースとGL BASEが我が家の可能性を広げてくれた。<br><br><br>
+                                そして、この自宅オフィスは私の隠れ家でもある。<br><br>
+                                平日はデスクとして使っているシートを折り畳めば<br>
+                                趣味のDIYを楽しめるガレージに早変わり。<br><br>
+                                妻が欲しいと言っていたスツールがそろそろ完成しそうだ。<br>
+                                これはなかなかの自信作。<br>
+                                明日こっそりリビングに置いておこう。
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wcm">
+                    <div class="content-Org">
+                        <h4 class="content-Org__heading">
+                            仕事がはかどる自分だけの空間
+                        </h4>
+                        <ul class="content-Org__txt">
+                            <li>自家用車がデスクに。玄関を出て数秒でワークスペースに入室完了。</li>
+                            <li>トイレや食事はすぐに自宅に戻れる利便性。</li>
+                            <li>シートを倒せば幅1.3mほどのテーブルが現れ、ノートPCと資料を広げても余裕のある広々とした
+                                作業スペースになる。</li>
+                            <li>車だからこその特性、ワークスペースが移動できるからワーケーションにも最適。</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="content content--04">
+                    <div class="wcm-content">
+                        <div class="wcm-content__inside">
+                            <div class="wcm-content__inside--ttl">
+                                プライベト空間<br>
+                                家族を助けてくれる
+                            </div>
+                            <div class="wcm-content__inside--txt">
+                                災害時に自宅で暮らすことが困難になった場合、<br>
+                                不特定多数の人々と共に<br>
+                                避難生活を送ることが予想される。<br><br>
+                                プライベートな空間がないことで<br>
+                                生活が不便になるだけでなく、<br>
+                                家族が大きなストレスを抱えることが心配だ。<br><br><br>
+                                そんな時にGL BASEがあれば、<br>
+                                寝る・食べる・トイレが使える・<br>
+                                仕事もできる。<br><br>
+                                いざという時に頼りになる<br>
+                                「我が家の避難所」だ。
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wcm">
+                    <div class="content-Org">
+                        <h4 class="content-Org__heading">
+                            利便性＋精神的な救い
+                        </h4>
+                        <ul class="content-Org__txt">
+                            <li>車内で本格的な就寝ができる（大人2名＋子ども3名程度）</li>
+                            <li>トイレにもなる（簡易トイレを設置可能）</li>
+                            <li>周りの視線が気になりやすい女性や子どもも安心して着替えられる</li>
+                            <li>エアコンを使うことができる</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="tree-Bg">
+                    <div class="tree-Bg__btn">
+                        <a class="tree-Bg__btn--custom" href="">開発ストーリー</a>
+                        <a class="tree-Bg__btn--custom" href="">仕様・価格</a>
+                    </div>
+                </div>
+                <div class="prev-footer">
+                    <div class="quote">
+                        <div class="quote__content">
+                            <div class="quote__content--ttl">
+                            避難拠点・防災拠点として<br>
+                            ご活用いただけます
+                            </div>
+                            <a class="quote__content--btn">
+                            自治体関係者様へ
+                            </a>
+                        </div>
+                        <div class="quote__content">
+                            <div class="quote__content--ttl">
+                            製品のお取り扱いを<br>
+                            ご希望の企業様はこちら
+                            </div>
+                            <a class="quote__content--btn">
+                            販売代理店募集
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
     </main>
     <?php include(APP_PATH . 'libs/footer.php'); ?>
 </body>
